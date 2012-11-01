12f1628debecbd1cbf39da0d1cf3a242e5aba9e3
Nguyen Tien Thinh <tienthinh2011@gmail.com>
2012-11-01 20:30:25 +0900

    Change the tukey function but git add -u! error heap

--- ERCP2v2/N2tEstimator.cpp
+++ ERCP2v2/N2tEstimator.cpp
@@ -4,6 +4,7 @@
 
 void N2tEstimator::estimate( cv::Mat& objPoints, cv::Mat& imgPoints, cv::Mat& camera_intrinsic, cv::Mat& distCoeffs,cv::Mat& rvec,cv::Mat& tvec, int mode, bool jac )
 {
+
 	N2tEstimatorData* n2tData = new N2tEstimatorData();
 	objPoints.copyTo(n2tData->ObjPoints);
 	imgPoints.copyTo(n2tData->ImgPoints);
@@ -14,11 +15,18 @@ void N2tEstimator::estimate( cv::Mat& objPoints, cv::Mat& imgPoints, cv::Mat& ca
 	double *_r = rvec.ptr<double>();
 	double *_t = tvec.ptr<double>();	
 	double p[6] = {_r[0], _r[1], _r[2],_t[0], _t[1], _t[2]};
-	double x[1];
+	double *x = new double[300];
 	cv::Mat errPoints;
 	cv::projectPoints(objPoints,rvec,tvec,camera_intrinsic,distCoeffs,errPoints);
 	errPoints = errPoints - imgPoints;
-	x[0] = cv::norm(errPoints);	
+	/*for (int j = 0; j<imgPoints.cols;j++)
+	{
+		float* data = imgPoints.ptr<float>(j);
+		for (int i = 0; i<imgPoints.rows; i++)
+		{
+			x[j*imgPoints.cols+i]  = (double)data[i];
+		}
+	}*/
 	int m, n;
 	double opts[LM_OPTS_SZ], info[LM_INFO_SZ]; 
 	opts[0] = LM_INIT_MU;// tau = LM_INIT_MU = 1E-3;
@@ -29,7 +37,7 @@ void N2tEstimator::estimate( cv::Mat& objPoints, cv::Mat& imgPoints, cv::Mat& ca
 
 	// tukey function
 	m = 6;
-	n = 1;
+	n = imgPoints.cols*imgPoints.rows;
 
 	double *work = new double;
 	double *covar = new double;
@@ -44,12 +52,13 @@ void N2tEstimator::estimate( cv::Mat& objPoints, cv::Mat& imgPoints, cv::Mat& ca
 	default:
 		break;
 	}
-
+	
 	
 	cv::Mat __rvec(3,1,CV_64F,p);
 	cv::Mat __tvec(3,1,CV_64F,p+3);
 	__rvec.copyTo(rvec);
 	__tvec.copyTo(tvec);
+	delete[] x;
 }
 
 N2tEstimator::N2tEstimator()
@@ -72,30 +81,34 @@ void leastSquare( double *p, double *x, int m, int n, void *adata )
 	cv::Mat errPoints;
 	cv::projectPoints(n2tData->ObjPoints,rvec,tvec,n2tData->camera_intrinsic,n2tData->distCoeffs,errPoints);	
 	errPoints = errPoints-n2tData->ImgPoints;
-	x[0] = cv::norm(errPoints);		
+	for (int j = 0; j<errPoints.cols;j++)
+	{
+		float* data = errPoints.ptr<float>(j);
+		for (int i = 0; i<errPoints.rows; i++)
+		{
+			x[j*errPoints.cols+i]  = data[i];
+		}
+	}
 }
 
 void tukey( double *p, double *x, int m, int n, void*adata )
 {
 	N2tEstimatorData* n2tData = (N2tEstimatorData*)adata;
-	x[0] = 0;
+
 	cv::Mat rvec(3,1,CV_64F,p);
 	cv::Mat tvec(3,1,CV_64F,p+3);
 	cv::Mat errPoints;
 	cv::projectPoints(n2tData->ObjPoints,rvec,tvec,n2tData->camera_intrinsic,n2tData->distCoeffs,errPoints);	
 	errPoints = errPoints-n2tData->ImgPoints;
+	double c = 2.0;
 	for (int j = 0; j<errPoints.cols;j++)
 	{
-		float* data = errPoints.ptr<float>(j);
-		double c = 2.0;
-		for (int i = 0; i<errPoints.rows;i++)
+		float* data = errPoints.ptr<float>(j);		
+		for (int i = 0; i<errPoints.rows; i++)
 		{
-			double err =  data[i];
-			if (err>c || err<-c){
-				x[0] += c*c;
-			}
-			else x[0] +=err*err;
+			if (data[i]>c) x[j*errPoints.cols+i] =c;
+			else if (data[i]<-c) x[j*errPoints.cols+i] =-c;
+			else x[j*errPoints.cols+i] = data[i];
 		}
-
-	}	
+	}
 }
